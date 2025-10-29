681acab728b9ce89f3c48b232fca39b5ff294a60
Sürya Têjä <11123sunny@gmail.com>
2025-10-29 11:39:23 +0000

    remove errors and fetch the api calls speed up and reduce the app size

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,5 +2,4 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/generate-initial-itinerary.ts';
-import '@/ai/flows/summarize-activity-details.ts';
 import '@/ai/tools/get-place-photo.ts';
