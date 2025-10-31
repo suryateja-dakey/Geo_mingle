cd8a18f156d6797ffc4852a432b591f54660cbc9
Sürya Têjä <11123sunny@gmail.com>
2025-10-31 13:06:45 +0000

    add animation while changing dark mode to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
           <Toaster />
